6c4aec25b95b4386d71e2b2e7717dbb714068c11
Richard Klein <richwklein@gmail.com>
2019-03-13 12:39:11 -0500

    Site metadata
    Get title and author information updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Aging Developer`,
+    author: `Richard Kleins`,
+    description: `TODO.`,
+    siteUrl: `https://www.agingdeveloper.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `richwklein`,
     },
   },
   plugins: [
