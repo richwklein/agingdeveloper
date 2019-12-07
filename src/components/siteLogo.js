79a2053da029009e05f29fc51de7e70618562b2b
Richard Klein <richwklein@gmail.com>
2019-12-07 10:40:04 -0600

    Add site navigation

--- src/components/siteLogo.js
+++ src/components/siteLogo.js
@@ -2,13 +2,13 @@ import React from "react";
 import Meta from "../meta";
 import image from "../images/logo-399x275.jpg";
 
-const Logo = () => {
+const SiteLogo = () => {
   const { siteTitle } = Meta();
   return (
-    <a href="/" title={siteTitle} class="site-logo">
+    <a href="/" title={siteTitle} className="site-logo">
       <img src={image} alt={siteTitle} width="399" height="275" />
     </a>
   );
 };
 
-export default Logo;
+export default SiteLogo;
