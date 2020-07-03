0becb1e77fde0a349deb8efee5210eebb55bb5b5
Richard Klein <richwklein@gmail.com>
2020-07-03 11:32:32 -0500

    Get theme colors set

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      light: "#406183",
+      main: "#0e3756",
+      dark: "#00112d",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      light: "#ffffff",
+      main: "#e2e2e2",
+      dark: "#b0b0b0",
+      contrastText: "#000000",
+    },
+  },
+});
