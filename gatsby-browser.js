434577fed2be7d1fd1d566bcbe5e8e2ccef8c91d
Richard Klein <richwklein@gmail.com>
2020-07-03 12:28:39 -0500

    fix ssr

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ThemeProvider } from "@material-ui/styles";
-import theme from "./src/style/theme";
+import theme from "./src/styles/theme";
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={theme}>{element}</ThemeProvider>
