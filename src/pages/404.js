9c6d6ce16fe97deda23982eaf3d62f2443d65d66
Rich Klein <31229235+richwklein@users.noreply.github.com>
2020-07-23 09:22:47 -0500

    Moving scroll logic into layout component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Box } from "@material-ui/core";
 import Layout from "../components/Layout";
-import { useHasScroll } from "has-scroll-hook";
 
 export default () => {
-  const hasScroll = useHasScroll();
 
   return (
-    <Layout hasScroll={hasScroll} showLogoImage={true}>
+    <Layout showLogoImage={true}>
       <Box flexGrow={1} marginX="auto" width="100%" maxWidth={1280}></Box>
     </Layout>
   );
