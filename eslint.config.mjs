9ffb9ded6ec5bc49301d69d2aee4e3e393e870f7
Richard Klein <31229235+richwklein@users.noreply.github.com>
2024-11-21 11:05:49 -0600

    Update dependencies to pick up typescript / eslint bug fix (#714)
    ## Proposed changes
    Dependency updates to pick up a bug fix from typescript-eslint
    
    ## Types of changes
    
    What types of changes does your code introduce?
    _Put an `x` in the boxes that apply_
    
    - [x] Bugfix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation Update (if none of the other choices apply)
    
    ## Checklist
    
    _Put an `x` in the boxes that apply. You can also fill these out after
    creating the PR. If you're unsure about any of them, don't hesitate to
    ask. We're here to help! This is simply a reminder of what we are going
    to look for before merging your code._
    
    - [x] Lint and unit tests pass locally with my changes
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] I have added the necessary documentation (if appropriate)
    - [x] Any dependent changes have been merged and published in downstream
    modules
    - [x] Link to issue to close it

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,13 +31,6 @@ export default [
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-expressions': [
-        'error',
-        {
-          allowShortCircuit: false,
-          allowTernary: false,
-        },
-      ], // TODO remove once eslint bug is fixed
       ...vitestLint.configs.recommended.rules,
       'vitest/prefer-lowercase-title': 'error',
     },
