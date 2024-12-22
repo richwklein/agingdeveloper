ea4228044831acc0a1341600dfbb325c0ccec8c0
Rich Klein <richwklein@gmail.com>
2024-12-22 11:02:49 -0600

    Add article tests

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -3,24 +3,20 @@ import authorData from 'src/content/data/author.json'
 import siteData from 'src/content/data/site.json'
 import { vi } from 'vitest'
 
-const getJson = (id: string, source: Array<Record<string, any>>): Record<string, any> => {
-  const data = source.find((entry) => entry.id == id)
-  if (!data) {
-    return {}
+const cleanEntry = (entry: Record<string, any>): Record<string, any> => {
+  if (entry.published) {
+    entry.published = new Date(entry.published)
   }
-  if (data.published) {
-    data.published = new Date(data.published)
+  if (entry.modified) {
+    entry.modified = new Date(entry.modified)
   }
-  if (data.modified) {
-    data.modified = new Date(data.modified)
-  }
-  if (data.avatar) {
-    data.avatar = {
-      src: data.avatar,
+  if (entry.avatar) {
+    entry.avatar = {
+      src: entry.avatar,
     }
   }
 
-  return data
+  return entry
 }
 
 const MockArticles = [
@@ -30,55 +26,144 @@ const MockArticles = [
     description: 'Mock article 1 description',
     featured: {
       image: {
-        src: 'mock-article-image.jpg',
+        src: 'mock-article-1-image.jpg',
       },
     },
-    published: new Date(),
-    category: 'mock-category',
-    tags: ['mock-tag'],
+    popular: false,
+    published: '2023-10-19',
+    author: { id: 'mock-author-1' },
+    category: 'mock-category-1',
+    tags: ['mock-tag-1', 'mock-tag-2', 'mock-tag-3', 'mock-tag-4'],
   },
   {
-    id: 'mock-article-with-modified',
+    id: 'mock-article-2',
+    title: 'Mock Article 2',
+    description: 'Mock article 2 description',
+    featured: {
+      image: {
+        src: 'mock-article-2-image.jpg',
+      },
+    },
+    popular: true,
+    published: '2023-08-01',
+    author: { id: 'mock-author-1' },
+    category: 'mock-category-1',
+    tags: ['mock-tag-1', 'mock-tag-2', 'mock-tag-3'],
+  },
+  {
+    id: 'mock-article-3',
+    title: 'Mock Article 3',
+    description: 'Mock article 3 description',
+    featured: {
+      image: {
+        src: 'mock-article-3-image.jpg',
+      },
+    },
+    popular: false,
+    published: '2024-05-01',
+    author: { id: 'mock-author-1' },
+    category: 'mock-category-1',
+    tags: ['mock-tag-1', 'mock-tag-2', 'mock-tag-3'],
+  },
+  {
+    id: 'mock-article-4', // article for related with different category
+    title: 'Mock Article 4',
+    description: 'Mock article 4 description',
+    featured: {
+      image: {
+        src: 'mock-article-4-image.jpg',
+      },
+    },
+    popular: false,
+    published: '2023-4-01',
+    author: { id: 'mock-author-1' },
+    category: 'mock-category-2',
+    tags: ['mock-tag-1', 'mock-tag-2', 'mock-tag-3'],
+  },
+  {
+    id: 'mock-article-with-modified', // article with modified and no related articles
     title: 'Mock Article with Modified',
     description: 'Mock article with modified description',
     featured: {
       image: {
-        src: 'mock-article-image.jpg',
+        src: 'mock-article-with-modified-image.jpg',
       },
     },
-    published: new Date(),
-    modified: new Date(),
-    category: 'mock-category',
-    tags: ['mock-tag'],
+    popular: true,
+    published: '2022-08-09',
+    modified: '2024-12-21',
+    category: 'mock-category-3',
+    author: { id: 'mock-author-2' },
+    tags: ['mock-tag-5'],
   },
 ]
 // Mock astro:content module
 vi.mock('astro:content', () => {
   return {
     getEntry: vi.fn(async (collection, id) => {
+      let source: Array<Record<string, any>> | undefined = undefined
+
       switch (collection) {
         case 'site':
-          return {
-            id: id,
-            collection: collection,
-            data: getJson(id, siteData),
-          }
+          source = siteData
+          break
+
         case 'author':
-          return {
-            id: id,
-            collection: collection,
-            data: getJson(id, authorData),
-          }
+          source = authorData
+          break
 
         case 'article':
-          return {
-            id: id,
-            collection: collection,
-            data: getJson(id, MockArticles),
-          }
+          source = MockArticles
+          break
+
         default:
-          return null // Simulate no entry found
+          break
       }
+
+      if (!source) {
+        return undefined
+      }
+
+      const entry = source.find((entry) => entry.id == id)
+      if (!entry) {
+        return undefined
+      }
+
+      return {
+        id: id,
+        collection: collection,
+        data: cleanEntry(entry),
+      }
+    }),
+    getCollection: vi.fn(async (collection) => {
+      let source: Array<Record<string, any>> | undefined = undefined
+
+      switch (collection) {
+        case 'site':
+          source = siteData
+          break
+
+        case 'author':
+          source = authorData
+          break
+
+        case 'article':
+          source = MockArticles
+          break
+
+        default:
+          break
+      }
+
+      if (!source) {
+        return undefined
+      }
+
+      return source.map((entry) => ({
+        id: entry.id,
+        collection: collection,
+        data: cleanEntry(entry),
+      }))
     }),
   }
 })
