89da23cb2c3f361f2ece164dc50b514fc2944c98
Rich Klein <richwklein@gmail.com>
2024-12-22 08:26:29 -0600

    Add tests for AuthorJson

---
+++ test/components/seo/AuthorJson.test.ts
@@ -0,0 +1,85 @@
+import AuthorJson from '@components/seo/AuthorJson.astro'
+import { getDefaultAuthor } from '@utils/author'
+import { buildUrl } from '@utils/misc'
+import { getDefaultSite } from '@utils/site'
+import { experimental_AstroContainer as AstroContainer } from 'astro/container'
+import type { CollectionEntry } from 'astro:content'
+import { beforeAll, describe, expect, test } from 'vitest'
+
+describe('authorJson', () => {
+  let author: CollectionEntry<'author'>
+  let site: CollectionEntry<'site'>
+
+  let linkedData: string
+
+  beforeAll(async () => {
+    site = await getDefaultSite()
+    author = await getDefaultAuthor()
+
+    linkedData = await render(site, author)
+  })
+
+  const render = async (site: CollectionEntry<'site'>, author: CollectionEntry<'author'>) => {
+    const container = await AstroContainer.create()
+    return await container.renderToString(AuthorJson, {
+      props: {
+        site: site,
+        author: author,
+        writeCount: 25,
+      },
+    })
+  }
+
+  test('that it contains an ld+json', async () => {
+    expect(linkedData).toContain('<script type="application/ld+json">')
+  })
+
+  test('that it contains a profile page', async () => {
+    expect(linkedData).toContain('"@type":"ProfilePage"')
+  })
+  test('that it contains a person', async () => {
+    expect(linkedData).toContain('"mainEntity":{')
+    expect(linkedData).toContain('"@type":"Person"')
+  })
+
+  test('that it contains the date created', async () => {
+    expect(linkedData).toContain(`"dateCreated":"${author.data.published.toISOString()}"`)
+  })
+
+  test('that it contains the date modified', async () => {
+    expect(linkedData).toContain(`"dateModified":"${author.data.published.toISOString()}"`)
+  })
+
+  test('that it contains the date modified when different from published', async () => {
+    const modifiedAuthor = await getDefaultAuthor()
+    modifiedAuthor.data.modified = new Date('2024-12-21')
+    const linkedData = await render(site, modifiedAuthor)
+
+    expect(linkedData).toContain(`"dateModified":"${modifiedAuthor.data.modified?.toISOString()}"`)
+  })
+
+  test('that it contains the author id', async () => {
+    expect(linkedData).toContain(`"identifier":"${author.id}"`)
+  })
+
+  test('that it contains the author name', async () => {
+    expect(linkedData).toContain(`"name":"${author.data.name}"`)
+  })
+
+  test('that it contains the write count', async () => {
+    expect(linkedData).toContain('"interactionType":"https://schema.org/WriteAction"')
+    expect(linkedData).toContain('"userInteractionCount":25')
+  })
+
+  test('that it contains the author avatar', async () => {
+    const url = buildUrl(author.data.avatar.src, site.data.origin).toString()
+    expect(linkedData).toContain(`"image":"${url}"`)
+  })
+
+  test('that it contains the author socials', async () => {
+    expect(linkedData).toContain('"sameAs":[')
+    author.data.socials?.forEach((social) => {
+      expect(linkedData).toContain(`"${social.url}"`)
+    })
+  })
+})
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,70 +1,91 @@
 // vitest.setup.ts
-import { DEFAULT_AUTHOR_ID } from '@utils/author'
-import { DEFAULT_SITE_ID } from '@utils/site'
 import authorData from 'src/content/data/author.json'
 import siteData from 'src/content/data/site.json'
 import { vi } from 'vitest'
+
+const getJson = (id: string, source: Array<Record<string, any>>): Record<string, any> => {
+  const data = source.find((entry) => entry.id == id)
+  if (!data) {
+    return {}
+  }
+  if (data.published) {
+    data.published = new Date(data.published)
+  }
+  if (data.modified) {
+    data.modified = new Date(data.modified)
+  }
+  if (data.avatar) {
+    data.avatar = {
+      src: data.avatar,
+    }
+  }
+
+  return data
+}
 
 // Mock astro:content module
 vi.mock('astro:content', () => {
   return {
     getEntry: vi.fn(async (collection, id) => {
-      if (collection === 'site' && id === DEFAULT_SITE_ID) {
-        return {
-          id: DEFAULT_SITE_ID,
-          collection: 'site',
-          data: siteData.find((entry) => entry.id == DEFAULT_SITE_ID),
-        }
-      } else if (collection == 'author' && id == DEFAULT_AUTHOR_ID) {
-        return {
-          id: DEFAULT_AUTHOR_ID,
-          collection: 'author',
-          data: authorData.find((entry) => entry.id == DEFAULT_AUTHOR_ID),
-        }
-      } else if (collection == 'article') {
-        switch (id) {
-          case 'mock-article-1':
-            return {
-              id: `${id}`,
-              collection: 'article',
-              data: {
-                title: 'Mock Article 1',
-                description: 'Mock article 1 description',
-                featured: {
-                  image: {
-                    src: 'mock-article-image.jpg',
-                  },
-                },
-                published: new Date(),
-                category: 'mock-category',
-                tags: ['mock-tag'],
-              },
-            }
+      switch (collection) {
+        case 'site':
+          return {
+            id: id,
+            collection: 'site',
+            data: getJson(id, siteData),
+          }
+        case 'author':
+          return {
+            id: id,
+            collection: 'author',
+            data: getJson(id, authorData),
+          }
 
-          case 'mock-article-with-modified':
-            return {
-              id: `${id}`,
-              collection: 'article',
-              data: {
-                title: 'Mock Article with Modified',
-                description: 'Mock article with modified description',
-                featured: {
-                  image: {
-                    src: 'mock-article-image.jpg',
+        case 'article':
+          switch (id) {
+            case 'mock-article-1':
+              return {
+                id: `${id}`,
+                collection: 'article',
+                data: {
+                  title: 'Mock Article 1',
+                  description: 'Mock article 1 description',
+                  featured: {
+                    image: {
+                      src: 'mock-article-image.jpg',
+                    },
                   },
+                  published: new Date(),
+                  category: 'mock-category',
+                  tags: ['mock-tag'],
                 },
-                published: new Date(),
-                modified: new Date(),
-                category: 'mock-category',
-                tags: ['mock-tag'],
-              },
-            }
+              }
 
-          default:
-            return null // Simulate no entry found
-        }
+            case 'mock-article-with-modified':
+              return {
+                id: `${id}`,
+                collection: 'article',
+                data: {
+                  title: 'Mock Article with Modified',
+                  description: 'Mock article with modified description',
+                  featured: {
+                    image: {
+                      src: 'mock-article-image.jpg',
+                    },
+                  },
+                  published: new Date(),
+                  modified: new Date(),
+                  category: 'mock-category',
+                  tags: ['mock-tag'],
+                },
+              }
+
+            default:
+              return null // Simulate no entry found
+          }
+        default:
+          return null // Simulate no entry found
       }
-      return null // Simulate no entry found
     }),
   }
 })
