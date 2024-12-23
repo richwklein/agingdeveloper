bc124c18e508ed61425e294dfdf9182959f367d4
Rich Klein <richwklein@gmail.com>
2024-12-23 14:03:53 -0600

    fix linting issue

--- test/utils/site.test.ts
+++ test/utils/site.test.ts
@@ -1,6 +1,5 @@
 import { DEFAULT_SITE_ID, getDefaultSite, getSiteById } from '@utils/site'
 import { describe, expect, test } from 'vitest'
-import { vi } from 'vitest'
 
 describe('getDefaultSite', () => {
   test('that it returns the site defined by the default id', async () => {
@@ -12,11 +11,13 @@ describe('getDefaultSite', () => {
 
 describe('getSiteById', () => {
   test('that it returns the site by id', async () => {
-    const site = getSiteById('agingdeveloper')
+    const site = getSiteById('mock-site-1')
     expect(site).toBeDefined()
   })
 
   test('that it throws if site is not found', async () => {
     await expect(getSiteById('nonexistent')).rejects.toThrow('Site nonexistent is required')
   })
+
+  test.todo('that it replaces the origin on local')
 })
