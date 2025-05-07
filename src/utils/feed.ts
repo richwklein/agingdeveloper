555432113be568d135a4e1bfbc785004d8035a4a
Rich Klein <richwklein@gmail.com>
2025-05-07 17:02:27 -0500

    more tweaks

--- src/utils/feed.ts
+++ src/utils/feed.ts
@@ -1,3 +1,4 @@
+import { getImage } from 'astro:assets'
 import { type CollectionEntry, getCollection } from 'astro:content'
 import { Feed } from 'feed'
 import MarkdownIt from 'markdown-it'
@@ -52,6 +53,9 @@ export const getFeed = async (): Promise<Feed> => {
     })
   })
 
+  // map of image urls that can be replaced in the content
+  const imageUrls = await buildImageUrls()
+
   // add an item for each article
   articles.map((article) => {
     const author = authors.filter(
@@ -59,6 +63,17 @@ export const getFeed = async (): Promise<Feed> => {
     )[0]
 
     const articleUrl = buildUrl(`/article/${article.id}`, site.data.origin).href
+
+    // convert the image path to something that can be looked up in the imageUrls map
+    const imagePath = article.data.featured.image.src
+      .split('/article/')[1]
+      .split('?')[0]
+      .replace(/^(\d{4})\/([^/]+)/, (_, year, rest) => {
+        return `${year}-${rest}`
+      })
+    const imageSrc = imageUrls.get(imagePath) || article.data.featured.image.src
+    console.log('imageSrc', imageSrc)
+
     return feed.addItem({
       title: article.data.title,
       id: articleUrl,
@@ -74,19 +89,25 @@ export const getFeed = async (): Promise<Feed> => {
         },
       ],
       image: {
-        url: escapeXmlAttr(buildUrl(article.data.featured.image.src, site.data.origin).href),
-        type: mime.getType(article.data.featured.image.src.split('?')[0]) || undefined,
+        url: escapeXmlAttr(buildUrl(imageSrc, site.data.origin).href),
+        type: mime.getType(imageSrc.split('?')[0]) || undefined,
       },
       content: sanitizeHtml(parser.render(article.body || ''), {
         allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
         transformTags: {
           img: (tagName, attribs) => {
             const { src } = attribs
+            let imageSrc = src
+            if (!src.startsWith('./')) {
+              const imagePath = `${article.id}/${src.replace(/^\.\//, '')}`
+              imageSrc = imageUrls.get(imagePath) || src
+            }
+
             return {
               tagName,
               attribs: {
                 ...attribs,
-                src: buildUrl(src, articleUrl).href,
+                src: buildUrl(imageSrc, site.data.origin).href,
               },
             }
           },
@@ -106,6 +127,36 @@ export const getFeed = async (): Promise<Feed> => {
   })
 
   return feed
+}
+
+/**
+ * Build a map of Image Urls
+ *
+ * This function will build a map of image urls for all images in the content directory.
+ * The keys will be the relative file path to the image starting after the article folder,
+ * and the values will be the relative url to the image.
+ *
+ * @returns a Map of image urls
+ */
+const buildImageUrls = async (): Promise<Map<string, string>> => {
+  const imageGlobs = import.meta.glob<{ default: ImageMetadata }>(
+    `/src/content/article/**/*.{jpeg,jpg,png,gif,svg,webp}`
+  )
+
+  const images: Map<string, string> = new Map()
+  for (const key of Object.keys(imageGlobs)) {
+    const src = key
+      .split('?')[0]
+      .replace(/^\/?src\/content\/article\//, '')
+      .replace(/^(\d{4})\/([^/]+)/, (_, year, rest) => {
+        return `${year}-${rest}`
+      })
+    const metadata = await imageGlobs[key]().then((result) => result.default)
+    const image = await getImage({ src: metadata })
+    console.log('image', image)
+    images.set(src, image.src)
+  }
+  return images
 }
 
 /**
